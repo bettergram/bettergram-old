f3f02b2befe908c15259109d525b66b0377f0dc6
Gilmanov Ildar <dev@ddwarf.org>
2018-05-24 10:29:43 +0700

    Add fields to CryptoPrice class

--- Telegram/SourceFiles/bettergram/cryptoprice.cpp
+++ Telegram/SourceFiles/bettergram/cryptoprice.cpp
@@ -10,8 +10,87 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 
 namespace Bettergram {
 
-CryptoPrice::CryptoPrice(QObject *parent) : QObject(parent)
+CryptoPrice::CryptoPrice(const QString &name, const QString &shortName, QObject *parent) :
+	QObject(parent),
+	_name(name),
+	_shortName(shortName)
 {
+}
+
+const QString &CryptoPrice::name() const
+{
+	return _name;
+}
+
+const QString &CryptoPrice::shortName() const
+{
+	return _shortName;
+}
+
+double CryptoPrice::currentPrice() const
+{
+	return _currentPrice;
+}
+
+QString CryptoPrice::currentPriceString() const
+{
+	if (_currentPrice < 1.0) {
+		return QString("$%1").arg(_currentPrice, 0, 'g', 4);
+	} else {
+		return QString("$%1").arg(_currentPrice, 0, 'g', 2);
+	}
+}
+
+void CryptoPrice::setCurrentPrice(double currentPrice)
+{
+	if (_currentPrice != currentPrice) {
+		_currentPrice = currentPrice;
+		emit currentPriceChanged();
+	}
+}
+
+double CryptoPrice::changeFor24Hours() const
+{
+	return _changeFor24Hours;
+}
+
+QString CryptoPrice::changeFor24HoursString() const
+{
+	return QString("%1%").arg(_changeFor24Hours, 0, 'g', 2);
+}
+
+void CryptoPrice::setChangeFor24Hours(double changeFor24Hours)
+{
+	if (_changeFor24Hours != changeFor24Hours) {
+		_changeFor24Hours = changeFor24Hours;
+		emit changeFor24HoursChanged();
+	}
+}
+
+bool CryptoPrice::isCurrentPriceGrown() const
+{
+	return _isCurrentPriceGrown;
+}
+
+void CryptoPrice::setIsCurrentPriceGrown(bool isCurrentPriceGrown)
+{
+	if (_isCurrentPriceGrown != isCurrentPriceGrown) {
+		_isCurrentPriceGrown = isCurrentPriceGrown;
+		emit currentPriceChanged();
+	}
+}
+
+bool CryptoPrice::isChangeFor24HoursGrown() const
+{
+	return _isChangeFor24HoursGrown;
+}
+
+void CryptoPrice::setIsChangeFor24HoursGrown(bool isChangeFor24HoursGrown)
+{
+	if (_isChangeFor24HoursGrown != isChangeFor24HoursGrown) {
+		_isChangeFor24HoursGrown = isChangeFor24HoursGrown;
+		emit isChangeFor24HoursGrownChanged();
+	}
 }
 
 } // namespace Bettergrams
--- Telegram/SourceFiles/bettergram/cryptoprice.h
+++ Telegram/SourceFiles/bettergram/cryptoprice.h
@@ -13,21 +13,60 @@ namespace Bettergram {
 
 /**
  * @brief The CryptoPrice class contains current price of one cryptocurrency.
+ * See also https://www.livecoinwatch.com
  */
 class CryptoPrice : public QObject {
 	Q_OBJECT
 
 public:
-	explicit CryptoPrice(QObject *parent = nullptr);
+	explicit CryptoPrice(const QString &name, const QString &shortName, QObject *parent = nullptr);
+
+	const QString &name() const;
+	const QString &shortName() const;
+
+	double currentPrice() const;
+	QString currentPriceString() const;
+	void setCurrentPrice(double currentPrice);
+
+	double changeFor24Hours() const;
+	QString changeFor24HoursString() const;
+	void setChangeFor24Hours(double changeFor24Hours);
+
+	bool isCurrentPriceGrown() const;
+	void setIsCurrentPriceGrown(bool isCurrentPriceGrown);
+
+	bool isChangeFor24HoursGrown() const;
+	void setIsChangeFor24HoursGrown(bool isChangeFor24HoursGrown);
 
 public slots:
 
 signals:
+	void currentPriceChanged();
+	void changeFor24HoursChanged();
+
+	void isCurrentPriceGrownChanged();
+	void isChangeFor24HoursGrownChanged();
 
 protected:
 
 private:
+	/// Name of the cryptocurrency. For example: Bitcoin
+	const QString _name;
 
+	/// Short name of the cryptocurrency. For example: BTC
+	const QString _shortName;
+
+	/// Current price of the cryptocurrency. For example: $7935.96
+	double _currentPrice;
+
+	/// Price change of the cryptocurrency for the latest 24 hours. For example: -3.22%
+	double _changeFor24Hours;
+
+	/// True if the current price of the cryptocurrency is grown
+	bool _isCurrentPriceGrown;
+
+	/// True if the price change of the cryptocurrency for the latest 24 hours is grown
+	bool _isChangeFor24HoursGrown;
 };
 
 } // namespace Bettergram
