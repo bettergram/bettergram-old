8977cb9901ffd348f829dd41be0b8ffe4051a16a
Gilmanov Ildar <dev@ddwarf.org>
2018-06-03 15:48:30 +0700

    Remove redundand commented code

--- Telegram/SourceFiles/chat_helpers/prices_list_widget.cpp
+++ Telegram/SourceFiles/chat_helpers/prices_list_widget.cpp
@@ -386,49 +386,6 @@ void PricesListWidget::paintEvent(QPaintEvent *event) {
 
 		top += st::pricesPanTableRowHeight;
 	}
-
-	//	QFont pricesPanSiteNameFont = QFont();
-	//	pricesPanSiteNameFont.setPixelSize(16);
-
-	//	painter.setFont(pricesPanSiteNameFont);
-
-	//	enumerateSections([this, &painter, r, fromColumn, toColumn](const SectionInfo &info) {
-	//		if (r.top() >= info.rowsBottom) {
-	//			return true;
-	//		} else if (r.top() + r.height() <= info.top) {
-	//			return false;
-	//		}
-	//		if (info.section > 0 && r.top() < info.rowsTop) {
-	//			painter.setFont(st::emojiPanHeaderFont);
-	//			painter.setPen(st::emojiPanHeaderFg);
-	//			painter.drawTextLeft(st::emojiPanHeaderLeft - st::buttonRadius, info.top + st::emojiPanHeaderTop, width(), lang(LangKey(lng_emoji_category0 + info.section)));
-	//		}
-	//		if (r.top() + r.height() > info.rowsTop) {
-	//			ensureLoaded(info.section);
-	//			auto fromRow = floorclamp(r.y() - info.rowsTop, _singleSize.height(), 0, info.rowsCount);
-	//			auto toRow = ceilclamp(r.y() + r.height() - info.rowsTop, _singleSize.height(), 0, info.rowsCount);
-	//			for (auto i = fromRow; i < toRow; ++i) {
-	//				for (auto j = fromColumn; j < toColumn; ++j) {
-	//					auto index = i * _columnCount + j;
-	//					if (index >= info.count) break;
-
-	//					auto selected = (!_picker->isHidden() && info.section * MatrixRowShift + index == _pickerSel) || (info.section * MatrixRowShift + index == _selected);
-
-	//					auto w = QPoint(_rowsLeft + j * _singleSize.width(), info.rowsTop + i * _singleSize.height());
-	//					if (selected) {
-	//						auto tl = w;
-	//						if (rtl()) tl.setX(width() - tl.x() - _singleSize.width());
-	//						App::roundRect(painter, QRect(tl, _singleSize), st::emojiPanHover, StickerHoverCorners);
-	//					}
-	//					auto sourceRect = QRect(_emoji[info.section][index]->x() * _esize, _emoji[info.section][index]->y() * _esize, _esize, _esize);
-	//					auto imageLeft = w.x() + (_singleSize.width() - (_esize / cIntRetinaFactor())) / 2;
-	//					auto imageTop = w.y() + (_singleSize.height() - (_esize / cIntRetinaFactor())) / 2;
-	//					painter.drawPixmapLeft(imageLeft, imageTop, width(), App::emojiLarge(), sourceRect);
-	//				}
-	//			}
-	//		}
-	//		return true;
-	//	});
 }
 
 void PricesListWidget::resizeEvent(QResizeEvent *e)
