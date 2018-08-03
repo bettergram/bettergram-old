f5095779c161c66ba51b3489c3e92079a543f188
Gilmanov Ildar <dev@ddwarf.org>
2018-08-03 19:54:00 +0700

    Fix create_dmg.sh script

--- Telegram/gyp/create_dmg.sh
+++ Telegram/gyp/create_dmg.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 # We have to do that in case we have installed Node js modules without sudo
 # See https://github.com/sindresorhus/guides/blob/master/npm-global-without-sudo.md
-[ -f ~/.bash_profile ] && source ~/.bash_profile && appdmg $1 $2
+
+if [ -f ~/.bash_profile ]; then
+  source ~/.bash_profile
+elif [ -f ~/.profile ]; then
+  source ~/.profile
+fi
+
+appdmg $1 $2
